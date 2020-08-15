6487d493c65508e40ccfd79c1261dc6c86762062
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-15 23:20:13 +0800

    forms

--- TestPro/NewsApp/forms.py
+++ TestPro/NewsApp/forms.py
@@ -2,7 +2,23 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Enter Username'
+                               }))
+    password = forms.CharField(max_length=100,
+                               widget=forms.PasswordInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Enter Password'
+                               }))
+    email = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Enter Email'
+                               }))
+    phone = forms.CharField(max_length=100,
+                                widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Enter Phone'
+                               }))
